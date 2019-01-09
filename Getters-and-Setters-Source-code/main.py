25e12f72d43968c53007bb3b63e70f3dfb4d2fe5
tvaditya <tvaditya@gmail.com>
2019-01-09 00:44:38 -0200

    Adding folders OOP
    New OOP  folders

---
+++ Getters-and-Setters-Source-code/main.py
@@ -0,0 +1,17 @@
+from player import Player
+
+tim = Player("Tim")
+
+print(tim.name)
+print(tim.lives)
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
---
+++ Subclasses-and-Overloading-Source-code/main.py
@@ -0,0 +1,10 @@
+from enemy import Enemy, Troll
+
+ugly_troll = Troll()
+print("Ugly troll - {}".format(ugly_troll))
+
+another_troll = Troll("Ug", 18, 1)
+print("Another troll - {}".format(another_troll))
+
+brother = Enemy("Urg", 23)
+print(brother)
